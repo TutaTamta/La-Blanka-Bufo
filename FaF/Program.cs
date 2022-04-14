911cabbbaeff26237068c3f4a2bdba8181073b8c
Stas <frolkovanasta@yandex.ru>
2022-04-14 20:31:36 +0300

    Лаба №5

---
+++ FaF/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace FaF
+{
+    class Program
+    {
+       static int[,] Mass(int x, int y)
+        {
+            int[,] mass = new int [x,y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    mass[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return mass;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во строк: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите кол-во столбцов: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] masA = new int [rows, cols];
+            int[,] masB = new int[rows, cols];
+            int[,] result = new int[rows, cols];
+
+            masA = Mass(rows,cols);
+            masB = Mass(rows, cols);
+
+            Console.WriteLine("Сложение - \"1\"");
+            Console.WriteLine("Вычитание - \"2\"");
+            int com = int.Parse(Console.ReadLine());
+
+            switch(com)
+            {
+                case 1:
+                    {
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                result[i, j] = masA[i, j] + masB[i, j];
+                            }
+                        }
+
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                Console.Write($"{result[i, j]} \t");
+                            }
+                            Console.WriteLine();
+                        }
+
+                        break;
+                    }
+
+                case 2:
+                    {
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                masB[i, j] = masB[i, j] * -1;
+                            }
+                        }
+
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                result[i, j] = masA[i, j] + masB[i, j];
+                            }
+                        }
+
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                Console.Write($"{result[i, j]} \t");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Shush/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Shush
+{
+    internal class Program
+    {
+        static int[] RandNum(int[] rand)
+        {
+            for (int i = 0; i < rand.Length; i++)
+            {
+                Random r = new Random();
+                rand[i] = r.Next(1, 100);
+            }
+            return rand;
+        }
+
+        static void PrintArr(int[] rand)
+        {
+            foreach (int i in rand)
+            {
+                Console.Write($"{i}\t");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] num = new int[100];
+            float sum = 0;
+            double mult = 1;
+
+            RandNum(num);
+            PrintArr(num);
+
+            Console.WriteLine("-----------------------------");
+            Console.Write("         Команда: ");
+            int comd = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------");
+
+            switch (comd)
+            {
+                case 0:
+                    {
+                        for (int i = 0; i < num.Length; i++)
+                        {
+                            sum += (float)num[i];
+                        }
+
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine($"| Среднее арифметическое равно: {sum / 100} |");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        break;
+                    }
+
+                case 1:
+                    {
+                        for (int i = 0; i < num.Length; i++)
+                        {
+                            mult *= num[i];
+                        }
+
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine($"| Среднее геометрическое равно: {Math.Pow(mult, 1/100.0)} |");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        for (int i = 0; i < num.Length; i++)
+                        {
+                            sum += (num[i] * num[i]);
+                        }
+
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine($"| Среднее квадратическое равно: {Math.Pow(sum / 100, 1 / 2.0)} |");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        break;
+                    }
+            }
+        }
+    }
+}
