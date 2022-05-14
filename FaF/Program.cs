13a4613d91ab0baa5130e1289a265d976b7bbcf7
Stas <frolkovanasta@yandex.ru>
2022-05-14 21:37:04 +0300

    Лаба №4

--- FaF/Program.cs
+++ FaF/Program.cs
@@ -44,6 +44,30 @@ namespace FaF
             }
         }
 
+        static int Minor(int[,] arr, int s, int f, int rows, int cols)
+        {
+            int minor = 0;
+            int c = 0;
+            int r = 0;
+            int[,] temp = new int [2, 2];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i != s && j != f)
+                    {
+                        temp[r, c] = arr[i, j];
+                        if (c < 1) { c++; }
+                        else if (r < 1) { r++; c = 0; }
+                    }
+                }
+            }
+
+            minor = temp[0, 0] * temp[1,1] - temp[0, 1] * temp[1, 0];
+
+            return minor;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("~ Если хотите найти обратную матрицу, первая матрица дожна быть квадратной и её ранг не должен быть больше трёх ~");
@@ -119,7 +143,7 @@ namespace FaF
                         {
                             //Нахождение об мат для 2 квад
 
-                            opr = masA[0, 0] + masA[1, 1] - masA[0, 1] - masA[1, 0];
+                            opr = masA[0, 0] * masA[1, 1] - masA[0, 1] * masA[1, 0];
 
                             if (opr != 0)
                             {
@@ -129,7 +153,6 @@ namespace FaF
                                 result[1, 1] = masA[0, 0];
 
                                 int k = 0;
-
                                 for (int i = 0; i < rows; i++)
                                 {
                                     for (int j = k; j < cols; j++)
@@ -149,21 +172,54 @@ namespace FaF
                             }
                             else
                             {
-                                Console.WriteLine("Невозможно найти обратную матрицу");
+                                Console.WriteLine("Невозможно найти обратную матрицу - определитель равен \"0\"");
                             }
                         }
                         else if (rows == cols && rows == 3)
                         {
                             //Нахождение обр матрицы для ранга 3
 
-                            opr = masA[0, 0] * (masA[1, 1] + masA[2, 2] - masA[2, 1] - masA[1, 2]) - masA[0, 1] * (masA[1, 0] + masA[2, 2] - masA[2, 0] - masA[1, 2] + masA[0, 2] * (masA[1, 0] + masA[2, 1] - masA[1, 1] + masA[2, 0]));
-                            // Не закончен
+                            opr = masA[0, 0] * Minor(masA,0,0,rows,cols) - masA[0, 1] * Minor(masA,0,1,rows,cols) + masA[0, 2] * Minor(masA,0,2,rows,cols);
+                            
+                            if (opr != 0)
+                            {
+                                int[,] souz = new int[rows, cols];
+                                for (int i = 0; i < rows; i++)
+                                {
+                                    for (int j = 0; j < cols; j++)
+                                    {
+                                        souz[i, j] = (int)Math.Pow(-1, i + j + 2) * Minor(masA, i, j, rows, cols);
+                                    }
+                                }
+
+                                int k = 0;
+                                for (int i = 0; i < rows; i++)
+                                {
+                                    for (int j = k; j < cols; j++)
+                                    {
+                                        if (i != j)
+                                        {
+                                            int temp = souz[i, j];
+                                            souz[i, j] = souz[j, i];
+                                            souz[j, i] = temp;
+                                        }
+                                    }
+                                    k++;
+                                }
+
+                                Console.WriteLine($"Обратная матрица: (1/{opr}) * ");
+                                Result(souz, rows, cols);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Невозможно найти обратную матрицу - определитель равен \"0\"");
+                            }
                         }
                         else if (rows > 3)
                         {
                             Console.WriteLine("Ранг матрицы больше трёх");
                         }
-                        else if (rows == cols)
+                        else if (rows != cols)
                         {
                             Console.WriteLine("Для нахождения обратной матрицы необходима квадратная матрица");
                         }
