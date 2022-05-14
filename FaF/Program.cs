70040f18c5c94215b2b8c78c7a4a9ebb59ecb9a4
Stas <frolkovanasta@yandex.ru>
2022-05-14 17:04:38 +0300

    Лаба №4
    Доп. задание - нахождение обратной матрицы

--- FaF/Program.cs
+++ FaF/Program.cs
@@ -4,9 +4,9 @@ namespace FaF
 {
     class Program
     {
-       static int[,] Mass(int x, int y)
+        static int[,] Mass(int x, int y)
         {
-            int[,] mass = new int [x,y];
+            int[,] mass = new int[x, y];
 
             for (int i = 0; i < x; i++)
             {
@@ -18,27 +18,60 @@ namespace FaF
             return mass;
         }
 
+        static void Result(int[,] result, int rows, int cols)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        Console.Write("~~~~~~~~~");
+                    }
+                    Console.WriteLine();
+                }
+
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{result[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write("~~~~~~~~~");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Введите кол-во строк: ");
+            Console.WriteLine("~ Если хотите найти обратную матрицу, первая матрица дожна быть квадратной и её ранг не должен быть больше трёх ~");
             Console.Write("Введите кол-во строк: ");
             int rows = int.Parse(Console.ReadLine());
 
             Console.Write("Введите кол-во столбцов: ");
             int cols = int.Parse(Console.ReadLine());
 
-            int[,] masA = new int [rows, cols];
+            int[,] masA = new int[rows, cols];
             int[,] masB = new int[rows, cols];
             int[,] result = new int[rows, cols];
 
-            masA = Mass(rows,cols);
+            Console.WriteLine("Введите числа первого массива");
+            masA = Mass(rows, cols);
+            Console.WriteLine("Введите числа второго массива");
             masB = Mass(rows, cols);
 
+            Console.WriteLine("-----------------------");
             Console.WriteLine("Сложение - \"1\"");
             Console.WriteLine("Вычитание - \"2\"");
+            Console.WriteLine("Обратная матрица (для первой матрицы) - \"3\"");
+            Console.Write("Команда: ");
             int com = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+            Console.WriteLine();
 
-            switch(com)
+            switch (com)
             {
                 case 1:
                     {
@@ -50,14 +83,7 @@ namespace FaF
                             }
                         }
 
-                        for (int i = 0; i < rows; i++)
-                        {
-                            for (int j = 0; j < cols; j++)
-                            {
-                                Console.Write($"{result[i, j]} \t");
-                            }
-                            Console.WriteLine();
-                        }
+                        Result(result, rows, cols);
 
                         break;
                     }
@@ -80,14 +106,73 @@ namespace FaF
                             }
                         }
 
-                        for (int i = 0; i < rows; i++)
+                        Result(result, rows, cols);
+
+                        break;
+                    }
+
+                case 3:
+                    {
+                        int opr = 0;
+
+                        if (rows == cols && rows == 2)
                         {
-                            for (int j = 0; j < cols; j++)
+                            //Нахождение об мат для 2 квад
+
+                            opr = masA[0, 0] + masA[1, 1] - masA[0, 1] - masA[1, 0];
+
+                            if (opr != 0)
                             {
-                                Console.Write($"{result[i, j]} \t");
+                                result[0, 0] = masA[1, 1];
+                                result[0, 1] = -masA[1, 0];
+                                result[1, 0] = -masA[0, 1];
+                                result[1, 1] = masA[0, 0];
+
+                                int k = 0;
+
+                                for (int i = 0; i < rows; i++)
+                                {
+                                    for (int j = k; j < cols; j++)
+                                    {
+                                        if (i != j)
+                                        {
+                                            int temp = result[i, j];
+                                            result[i, j] = result[j, i];
+                                            result[j, i] = temp;
+                                        }
+                                    }
+                                    k++;
+                                }
+
+                                Console.WriteLine($"Обратная матрица: (1/{opr}) * ");
+                                Result(result, rows, cols);
                             }
-                            Console.WriteLine();
+                            else
+                            {
+                                Console.WriteLine("Невозможно найти обратную матрицу");
+                            }
                         }
+                        else if (rows == cols && rows == 3)
+                        {
+                            //Нахождение обр матрицы для ранга 3
+
+                            opr = masA[0, 0] * (masA[1, 1] + masA[2, 2] - masA[2, 1] - masA[1, 2]) - masA[0, 1] * (masA[1, 0] + masA[2, 2] - masA[2, 0] - masA[1, 2] + masA[0, 2] * (masA[1, 0] + masA[2, 1] - masA[1, 1] + masA[2, 0]));
+                            // Не закончен
+                        }
+                        else if (rows > 3)
+                        {
+                            Console.WriteLine("Ранг матрицы больше трёх");
+                        }
+                        else if (rows == cols)
+                        {
+                            Console.WriteLine("Для нахождения обратной матрицы необходима квадратная матрица");
+                        }
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Некорректная команда");
                         break;
                     }
             }
